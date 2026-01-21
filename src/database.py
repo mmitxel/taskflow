c71b9dbc43e13ebdf8406b504d8549e34ceec1ee
Mauricio Michel <mauriciomichel@gmail.com>
2026-01-20 20:58:36 -0600

    Added database creation and test functions

--- src/database.py
+++ src/database.py
@@ -26,7 +26,7 @@ def crear_tablas():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tareas (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nombre TEXT NOT NULL,
+            titulo TEXT NOT NULL,
             descripcion TEXT,
             fecha_creacion TEXT,
             fecha_limite TEXT,
@@ -83,3 +83,66 @@ class DBManager:
         filas = cursor.fetchall()
         conn.close()
 
+        proyectos = [
+            Proyecto(nombre=fila['nombre'], descripcion=fila['descripcion'],
+                     id=fila['id'], estado=fila['estado'])
+            for fila in filas
+        ]
+        return proyectos
+
+    def obtener_tareas(self, estado=None):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        sql = 'SELECT * FROM tareas'
+        params = []
+
+        if estado:
+            sql += ' WHERE estado = ?'
+            params.append(estado)
+
+        sql += ' ORDER BY fecha_creacion ASC'
+
+        cursor.execute(sql, params)
+        filas = cursor.fetchall()
+        conn.close()
+
+        tareas = []
+
+        for fila in filas:
+            t = Tarea(
+                titulo=fila['titulo'],
+                fecha_limite=fila['fecha_creacion'],
+                fecha_creacion=fila['fecha_creacion'],
+                prioridad=fila['prioridad'],
+                proyecto_id=fila['proyecto_id'],
+                estado=fila['estado'],
+                descripcion=fila['descripcion']
+            )
+            tareas.append(t)
+
+        return tareas
+
+
+if __name__ == '__main__':
+    # Bloque de prueba para la clase
+    if os.path.exists(DATABASE_NAME):
+        os.remove(DATABASE_NAME)
+        print(f"Base de datos {DATABASE_NAME} eliminada.")
+
+    crear_tablas()
+    print(
+        f"Base de datos {DATABASE_NAME} y tablas inicializadas correctamente.")
+
+    # Prueba del CRUD (CREATE)
+    manager = DBManager()
+    tarea_prueba = Tarea(
+        titulo="Completar Ejercicio de CRUD",
+        fecha_limite="2025-10-30",
+        prioridad="Alta",
+        proyecto_id=0,
+        descripcion="Implementar el módulo database.py"
+    )
+
+    tarea_creada = manager.crear_tarea(tarea_prueba)
+    print(f"Tarea creada y ID asignado: {tarea_creada.id}")
